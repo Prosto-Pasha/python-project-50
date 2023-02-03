d30cb11197be403d4c4f4487e39381867aedb305
Pavel Belyavskiy <prostopasha@gmail.com>
2023-02-03 16:44:54 +0200

    update test_gendiff.py add tests for plain format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -44,6 +44,10 @@ def get_file4_yml():
 
 @pytest.fixture
 def correct_result1():
+    """
+    Результат сравнения файлов 1 и 2
+    (json и yaml) в формате stylish
+    """
     return '''{
   - follow: false
     host: hexlet.io
@@ -56,6 +60,10 @@ def correct_result1():
 
 @pytest.fixture
 def correct_result2():
+    """
+    Результат сравнения файлов 3 и 4
+    (json и yaml) в формате stylish
+    """
     return '''{
     common: {
       + follow: false
@@ -102,13 +110,46 @@ def correct_result2():
 }'''
 
 
+@pytest.fixture
+def correct_result3():
+    """
+    Результат сравнения файлов 1 и 2
+    (json и yaml) в формате plain
+    """
+    return '''Property 'follow' was removed
+Property 'proxy' was removed
+Property 'timeout' was updated. From 50 to 20
+Property 'verbose' was added with value: true'''
+
+
+@pytest.fixture
+def correct_result4():
+    """
+    Результат сравнения файлов 3 и 4
+    (json и yaml) в формате plain
+    """
+    return '''Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]'''
+
+
 def test_generate_diff_json(
         get_file1_json,
         get_file2_json,
         correct_result1
 ):
     '''
-    Тест функции generate_diff с тестовыми файлами JSON
+    Тест функции generate_diff
+    с тестовыми файлами JSON 1 и 2
+    формат stylish
     '''
     format = 'stylish'
     result = generate_diff(get_file1_json, get_file2_json, format)
@@ -120,12 +161,44 @@ def test_2_generate_diff_json(
         get_file4_json,
         correct_result2
 ):
-    '''
-    Тест 2 функции generate_diff с тестовыми файлами JSON 3 и 4
-    '''
+    """
+    Тест 2 функции generate_diff
+    с тестовыми файлами JSON 3 и 4
+    формат stylish
+    """
     format = 'stylish'
     result = generate_diff(get_file3_json, get_file4_json, format)
     assert result == correct_result2
+
+
+def test_3_generate_diff_json(
+        get_file1_json,
+        get_file2_json,
+        correct_result3
+):
+    """
+    Тест 3 функции generate_diff
+    с тестовыми файлами JSON 1 и 2
+    формат plain
+    """
+    format = 'plain'
+    result = generate_diff(get_file1_json, get_file2_json, format)
+    assert result == correct_result3
+
+
+def test_4_generate_diff_json(
+        get_file3_json,
+        get_file4_json,
+        correct_result4
+):
+    """
+    Тест 4 функции generate_diff
+    с тестовыми файлами JSON 3 и 4
+    формат plain
+    """
+    format = 'plain'
+    result = generate_diff(get_file3_json, get_file4_json, format)
+    assert result == correct_result4
 
 
 def test_generate_diff_yml(
@@ -133,25 +206,59 @@ def test_generate_diff_yml(
         get_file2_yml,
         correct_result1
 ):
-    '''
-    Тест функции generate_diff с тестовыми файлами YML, YAML
-    '''
+    """
+    Тест 1 функции generate_diff
+    с тестовыми файлами YML, YAML 1 и 2
+    default формат (stylish)
+    """
     # default format
     result = generate_diff(get_file1_yml, get_file2_yml)
     assert result == correct_result1
 
 
-def test2_generate_diff_yml(
+def test_2_generate_diff_yml(
         get_file3_yml,
         get_file4_yml,
         correct_result2
 ):
-    '''
-    Тест 2 функции generate_diff с тестовыми файлами YML, YAML 3 и 4
-    '''
+    """
+    Тест 2 функции generate_diff
+    с тестовыми файлами YML, YAML 3 и 4
+    формат stylish
+    """
     format = 'stylish'
     result = generate_diff(get_file3_yml, get_file4_yml, format)
     assert result == correct_result2
+
+
+def test_3_generate_diff_yml(
+        get_file1_yml,
+        get_file2_yml,
+        correct_result3
+):
+    """
+    Тест 3 функции generate_diff
+    с тестовыми файлами YML, YAML 1 и 2
+    формат plain
+    """
+    format = 'plain'
+    result = generate_diff(get_file1_yml, get_file2_yml, format)
+    assert result == correct_result3
+
+
+def test_4_generate_diff_yml(
+        get_file3_yml,
+        get_file4_yml,
+        correct_result4
+):
+    """
+    Тест 4 функции generate_diff
+    с тестовыми файлами YML, YAML 3 и 4
+    формат plain
+    """
+    format = 'plain'
+    result = generate_diff(get_file3_yml, get_file4_yml, format)
+    assert result == correct_result4
 
 
 def test_parser():
