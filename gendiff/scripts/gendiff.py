50a4778da38e43fdbd41f21601adb6d1f2814f23
Pavel Belyavskiy <prostopasha@gmail.com>
2023-02-07 18:27:21 +0200

    Refactor compare_files.py move from scripts

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,21 +1,21 @@
-from gendiff.scripts.compare_files import compare_files
+from gendiff.format_scripts.compare_files import compare_files
 import argparse
 import sys
 
 
 def generate_diff_cli():
-    '''
+    """
     Сравнивает два файла и выводит результат сравнения
-    '''
+    """
     options = parseargs(sys.argv[1:])
     args = vars(options).values()
     print(generate_diff(*args))
 
 
 def parseargs(args):
-    '''
+    """
     Возвращает аргументы, указанные при запуске
-    '''
+    """
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
@@ -33,10 +33,10 @@ def parseargs(args):
 
 
 def generate_diff(file_path1, file_path2, format='stylish'):
-    '''
+    """
     Получает пути к двум файлам,
     возвращает строку с результатом сравнения файлов
-    '''
+    """
     file1_ext = file_path1[-4:].upper()
     file2_ext = file_path1[-4:].upper()
     yaml_ext = ('.YML', 'YAML')
