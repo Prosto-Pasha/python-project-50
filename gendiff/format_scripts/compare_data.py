921ff53a9fb1266db6c94e22bcec82203a6388e4
Pavel Belyavskiy <prostopasha@gmail.com>
2023-02-13 16:32:31 +0200

    Refactor compare_data.py rename vars

--- gendiff/format_scripts/compare_data.py
+++ gendiff/format_scripts/compare_data.py
@@ -11,18 +11,18 @@ def get_diff_for_key(dict1, dict2, key):
     dict1 и dict2 по ключу key, возвращает
     список кортеж с результатом сравнения
     """
-    arg1 = get_item(dict1, key)
-    arg2 = get_item(dict2, key)
-    if arg1 == arg2:
-        result = ('unchanged', key, arg1, arg2)
-    elif arg1 is None:
-        result = ('added', key, arg1, arg2)
-    elif arg2 is None:
-        result = ('removed', key, arg1, arg2)
-    elif isinstance(arg1, dict) and isinstance(arg2, dict):
-        result = ('nested', key, get_diff_list(arg1, arg2), None)
+    old_data = get_item(dict1, key)
+    new_data = get_item(dict2, key)
+    if old_data == new_data:
+        result = ('unchanged', key, old_data, new_data)
+    elif old_data is None:
+        result = ('added', key, old_data, new_data)
+    elif new_data is None:
+        result = ('removed', key, old_data, new_data)
+    elif isinstance(old_data, dict) and isinstance(new_data, dict):
+        result = ('nested', key, get_diff_list(old_data, new_data), None)
     else:
-        result = ('changed', key, arg1, arg2)
+        result = ('changed', key, old_data, new_data)
     return result
 
 
